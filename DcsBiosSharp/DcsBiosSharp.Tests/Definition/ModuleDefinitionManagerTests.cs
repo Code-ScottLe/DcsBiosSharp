00d520b20299b86187588df69069c76fae3b31e7
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2020-02-21 03:06:41 -0800

    Add basic module and first test

---
+++ DcsBiosSharp/DcsBiosSharp.Tests/Definition/ModuleDefinitionManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DcsBiosSharp.Definition.Tests
+{
+    [TestClass]
+    public class ModuleDefinitionManagerTests
+    {
+        [TestMethod]
+        public async Task RefreshModuleAsyncTest_WithDefaultFolder_ReturnNonZeroModules()
+        {
+            // Arrange
+            var manager = new ModuleDefinitionManager();
+
+            // Act
+            await manager.RefreshModuleAsync();
+
+            // Assert
+            Assert.AreNotEqual(notExpected: 0, actual: manager.Modules.Count);
+        }
+    }
+}
---
+++ DcsBiosSharp/DcsBiosSharp/Definition/ModuleDefinitionManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DcsBiosSharp.Definition
+{
+    public class ModuleDefinitionManager
+    {
+        public static readonly string DEFAULT_DCS_BIOS_MODULE_DEFINITION_LOCATION = Environment.ExpandEnvironmentVariables("%USERPROFILE%") + @"/AppData/Roaming/DCS-BIOS/control-reference-json/";
+        public const string DEFAULT_MODULE_FOLDER_SEARCH_PATTERN = @"*.json";
+
+        public string ModuleDefinitionLocation
+        {
+            get; private set;
+        }
+
+        public IList<IModule> Modules
+        {
+            get; private set;
+        }
+
+        public IDcsBiosModuleDefinitionJsonParser Parser
+        {
+            get; private set;
+        }
+
+        public ModuleDefinitionManager()
+            : this (DEFAULT_DCS_BIOS_MODULE_DEFINITION_LOCATION, new DcsBiosModuleDefinitionJsonParser())
+        {
+        }
+
+        public ModuleDefinitionManager(string moduleDefinitionsLocation , IDcsBiosModuleDefinitionJsonParser parser)
+        {
+            ModuleDefinitionLocation = moduleDefinitionsLocation;
+            Parser = parser;
+            Modules = new List<IModule>();
+        }
+
+        public async Task RefreshModuleAsync(string searchPattern = DEFAULT_MODULE_FOLDER_SEARCH_PATTERN)
+        {
+            DirectoryInfo info = new DirectoryInfo(ModuleDefinitionLocation);
+            if(!info.Exists)
+            {
+                // should we throw?
+                return;
+            }
+
+            // look for all jsons
+            FileInfo[] files = info.GetFiles(searchPattern);
+
+            foreach(var file in files)
+            {
+                string moduleId = Path.GetFileNameWithoutExtension(file.FullName);
+
+                using (StreamReader streamReader = file.OpenText())
+                {
+                    string json = await streamReader.ReadToEndAsync();
+                    IModule module = await Task.Run(() => Parser.ParseModuleFromJson(moduleId, json));
+                    Modules.Add(module);
+                }
+            }
+        }
+    }
+}
