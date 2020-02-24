8f8758bae127013534a3fa0909e7984bf2cecb7c
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2020-02-24 14:47:28 -0800

    Add tests OutputDefinition

---
+++ DcsBiosSharp/DcsBiosSharp.Tests/Definition/Inputs/ActionCommandDefinitionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DcsBiosSharp.Connection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace DcsBiosSharp.Definition.Inputs.Tests
+{
+    [TestClass]
+    public class ActionCommandDefinitionTests
+    {
+        [TestMethod]
+        public void CreateCommandTest_WithValidArgs_ReturnExpectedCommandInstance()
+        {
+            // Arrange
+            var mockInstrument = new Mock<IModuleInstrument>();
+            mockInstrument.Setup(m => m.Identifier).Returns("TEST_INS");
+            var instance = new ActionCommandDefinition(mockInstrument.Object, "TOGGLE");
+
+            // Act
+            IDcsBiosCommand command = instance.CreateCommand();
+
+            // Assert
+            Assert.AreEqual(expected: "TOGGLE", actual: command.Arguments);
+            Assert.AreEqual(expected: "TEST_INS", actual: command.CommandIdentifier);
+        }
+    }
+}
