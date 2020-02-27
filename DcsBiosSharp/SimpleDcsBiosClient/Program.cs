4d2363cac54aebc0d4374a6ccf5ec413b0ba70b9
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2020-02-27 03:53:00 -0800

    Update the readme + flip the bool on exportBuffer

--- DcsBiosSharp/SimpleDcsBiosClient/Program.cs
+++ DcsBiosSharp/SimpleDcsBiosClient/Program.cs
@@ -24,7 +24,7 @@ namespace SimpleDcsBiosClient
 
             int sizeCounter = 0;
 
-            bool exportBuffer = true;
+            bool exportBuffer = false;
 
             DcsBiosClient client = new DcsBiosClient();
 
